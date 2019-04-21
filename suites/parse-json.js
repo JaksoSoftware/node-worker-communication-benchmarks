1529e66800c73b73dd847c0ac177dbb461531892
Olli Salli <olli@jakso.me>
2019-04-21 16:50:32 +0300

    Benchmark JSON.parse in threads spread to all CPUs, finally faster, but heavy-weight

--- suites/parse-json.js
+++ suites/parse-json.js
@@ -1,4 +1,5 @@
 const { Worker } = require('worker_threads')
+const os = require('os')
 
 const { Suite } = require('benchmark')
 const prettierBytes = require('prettier-bytes')
@@ -10,13 +11,15 @@ module.exports = Object.entries(allData).reduce((suites, [ size, data ]) => {
   const times = Math.round((10 * 1024 * 1024) / byteSize)
   const totalSize = times * byteSize
 
-  let worker = null
+  const workers = []
 
   return [
     ...suites,
     new Suite(`Parse ${prettierBytes(totalSize)} of ${size} ${prettierBytes(byteSize)} JSON strings`)
       .on('start', () => {
-        worker = new Worker('./workers/parse-json.js')
+        for (let i = 0; i < os.cpus().length; ++i) {
+          workers.push(new Worker('./workers/parse-json.js'))
+        }
       })
       .add('in main thread, from JSON byte buffer', () => {
         for (let i = 0; i < times; ++i) {
@@ -32,9 +35,9 @@ module.exports = Object.entries(allData).reduce((suites, [ size, data ]) => {
       })
       .add('in single worker thread, from copied JSON string, one at a time', async deferred => {
         for (let i = 0; i < times; ++i) {
-          worker.postMessage(data.asJSONString)
+          workers[0].postMessage(data.asJSONString)
           await new Promise(resolve => {
-            worker.once('message', parsed => {
+            workers[0].once('message', parsed => {
               verifyResult(parsed)
               resolve()
             })
@@ -46,14 +49,14 @@ module.exports = Object.entries(allData).reduce((suites, [ size, data ]) => {
         let repliesReceived = 0
 
         for (let i = 0; i < times; ++i) {
-          worker.postMessage(data.asJSONString)
+          workers[0].postMessage(data.asJSONString)
         }
-        worker.on('message', receiveReply)
+        workers[0].on('message', receiveReply)
 
         function receiveReply(parsed) {
           verifyResult(parsed)
           if (++repliesReceived === times) {
-            worker.off('message', receiveReply)
+            workers[0].off('message', receiveReply)
             deferred.resolve()
           }
         }
@@ -62,21 +65,37 @@ module.exports = Object.entries(allData).reduce((suites, [ size, data ]) => {
         let repliesReceived = 0
 
         for (let i = 0; i < times; ++i) {
-          worker.postMessage(data.asSharedJSONArray)
+          workers[0].postMessage(data.asSharedJSONArray)
         }
-        worker.on('message', receiveReply)
+        workers[0].on('message', receiveReply)
 
         function receiveReply(parsed) {
           verifyResult(parsed)
           if (++repliesReceived === times) {
-            worker.off('message', receiveReply)
+            workers[0].off('message', receiveReply)
+            deferred.resolve()
+          }
+        }
+      }, { defer: true })
+      .add('spread across threads for each CPU, from copied JSON string, pipelined', deferred => {
+        let repliesReceived = 0
+
+        for (let i = 0; i < times; ++i) {
+          workers[i % workers.length].postMessage(data.asJSONString)
+        }
+        workers.forEach(worker => worker.on('message', receiveReply))
+
+        function receiveReply(parsed) {
+          verifyResult(parsed)
+          if (++repliesReceived === times) {
+            workers.forEach(worker => worker.off('message', receiveReply))
             deferred.resolve()
           }
         }
       }, { defer: true })
       .on('complete', () => {
-        worker.unref()
-        worker = null
+        workers.forEach(worker => worker.unref())
+        workers.length = 0
       })
   ]
 
