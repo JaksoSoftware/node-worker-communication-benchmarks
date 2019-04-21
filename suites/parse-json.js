f04ce527313aa6e427b1c633473f67cc42b3dc36
Olli Salli <olli@jakso.me>
2019-04-21 07:50:28 +0300

    More understandable output with ~10MB total of JSON parsed for each size

--- suites/parse-json.js
+++ suites/parse-json.js
@@ -1,15 +1,23 @@
 const { Suite } = require('benchmark')
-const prettyBytes = require('pretty-bytes')
+const prettierBytes = require('prettier-bytes')
 
 const allData = require('../data')
 
-module.exports = Object.entries(allData).reduce((suites, [ size, data ]) => ([
-  ...suites,
-  new Suite(`Parse ${size} JSON string (${prettyBytes(data.asJSONString.length)})`)
-    .add('in main thread', () => {
-      const parsed = JSON.parse(data.asJSONString)
-      if (parsed.length !== data.asArray.length) {
-        throw new Error('unexpected result length')
-      }
-    })
-]), [])
+module.exports = Object.entries(allData).reduce((suites, [ size, data ]) => {
+  const byteSize = data.asJSONString.length
+  const times = Math.round((10 * 1024 * 1024) / byteSize)
+  const totalSize = times * byteSize
+
+  return [
+    ...suites,
+    new Suite(`Parse ${prettierBytes(totalSize)} of ${size} ${prettierBytes(byteSize)} JSON strings`)
+      .add('in main thread', () => {
+        for (let i = 0; i < times; ++i) {
+          const parsed = JSON.parse(data.asJSONString)
+          if (parsed.length !== data.asArray.length) {
+            throw new Error('unexpected result length')
+          }
+        }
+      })
+  ]
+}, [])
