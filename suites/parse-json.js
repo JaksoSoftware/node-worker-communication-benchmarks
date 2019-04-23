d8645f2f21c336e506a2bd0fa7d46bee3a580ebf
Olli Salli <olli@jakso.me>
2019-04-23 09:37:16 +0300

    Schedule all parses in all benchmark types with a chance for events to be processed in between
    Also the postMessages for pipelined worker thread parses

--- suites/parse-json.js
+++ suites/parse-json.js
@@ -22,35 +22,19 @@ module.exports = Object.entries(allData).reduce((suites, [ size, data ]) => {
           workers.push(new Worker('./workers/parse-json.js'))
         }
       })
-      .add('in main thread, from JSON byte buffer', deferred => {
-        let timesParsed = 0
-        doRemainingParses()
-
-        function doRemainingParses() {
+      .add('in main thread, from JSON byte buffer', async deferred => {
+        await callNTimesWithSetImmediate(times, () => {
           const parsed = JSON.parse(data.asJSONBuffer)
           verifyResult(parsed)
-
-          if (++timesParsed === times) {
-            deferred.resolve()
-          } else {
-            setImmediate(doRemainingParses)
-          }
-        }
+        })
+        deferred.resolve()
       }, { defer: true })
-      .add('in main thread, from JSON string', deferred => {
-        let timesParsed = 0
-        doRemainingParses()
-
-        function doRemainingParses() {
-          const parsed = JSON.parse(data.asJSONBuffer)
+      .add('in main thread, from JSON string', async deferred => {
+        await callNTimesWithSetImmediate(times, () => {
+          const parsed = JSON.parse(data.asJSONString)
           verifyResult(parsed)
-
-          if (++timesParsed === times) {
-            deferred.resolve()
-          } else {
-            setImmediate(doRemainingParses)
-          }
-        }
+        })
+        deferred.resolve()
       }, { defer: true })
       .add('in single worker thread, from copied JSON string, one at a time', async deferred => {
         for (let i = 0; i < times; ++i) {
@@ -62,11 +46,11 @@ module.exports = Object.entries(allData).reduce((suites, [ size, data ]) => {
       }, { defer: true })
       .add('in single worker thread, from copied JSON string, pipelined', deferred => {
         let repliesReceived = 0
-
-        for (let i = 0; i < times; ++i) {
-          workers[0].postMessage(data.asJSONString)
-        }
         workers[0].on('message', receiveReply)
+
+        callNTimesWithSetImmediate(times, () => {
+          workers[0].postMessage(data.asJSONString)
+        })
 
         function receiveReply(parsed) {
           verifyResult(parsed)
@@ -78,11 +62,11 @@ module.exports = Object.entries(allData).reduce((suites, [ size, data ]) => {
       }, { defer: true })
       .add('in single worker thread, with JSON in SharedArrayBuffer, pipelined', deferred => {
         let repliesReceived = 0
-
-        for (let i = 0; i < times; ++i) {
-          workers[0].postMessage(data.asSharedJSONArray)
-        }
         workers[0].on('message', receiveReply)
+
+        callNTimesWithSetImmediate(times, () => {
+          workers[0].postMessage(data.asSharedJSONArray)
+        })
 
         function receiveReply(parsed) {
           verifyResult(parsed)
@@ -94,11 +78,11 @@ module.exports = Object.entries(allData).reduce((suites, [ size, data ]) => {
       }, { defer: true })
       .add('spread across threads for each CPU, from copied JSON string, pipelined', deferred => {
         let repliesReceived = 0
-
-        for (let i = 0; i < times; ++i) {
-          workers[i % workers.length].postMessage(data.asJSONString)
-        }
         workers.forEach(worker => worker.on('message', receiveReply))
+
+        callNTimesWithSetImmediate(times, nCallsRemaining => {
+          workers[nCallsRemaining % workers.length].postMessage(data.asSharedJSONArray)
+        })
 
         function receiveReply(parsed) {
           verifyResult(parsed)
@@ -132,3 +116,18 @@ module.exports = Object.entries(allData).reduce((suites, [ size, data ]) => {
     }
   }
 }, [])
+
+async function callNTimesWithSetImmediate(n, workFn) {
+  await new Promise(resolve => {
+    callNFurtherTimes(n)
+
+    function callNFurtherTimes(nCallsRemaining) {
+      if (nCallsRemaining >= 1) {
+        workFn(nCallsRemaining)
+        setImmediate(callNFurtherTimes, nCallsRemaining - 1)
+      } else {
+        resolve()
+      }
+    }
+  })
+}
